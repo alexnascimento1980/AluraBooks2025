d856041d11e00ee5b842a22b4d16491564cb18ae
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-18 19:18:52 -0300

    aula 2

--- app/main.js
+++ app/main.js
@@ -1,30 +1,10 @@
-let livros = [];
-const endpointDaApi =
-  "https://guilhermeonrails.github.io/casadocodigo/livros.json";
-getBuscaLivrosDaAPI();
-const elementoParaInserirLivros = document.getElementById("livros");
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
 
-
-async function getBuscaLivrosDaAPI() {
-  const res = await fetch(endpointDaApi);
-  livros = await res.json();
-  exibirOsLivrosNaTela(livros);
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
-
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach((livro) => {
-    elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}"alt="${livro.alt}" />
-            <h2 class="livro__titulo">${livro.titulo}</h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
-            <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-            </div>
-        </div>
-        `
-    })
-  
-}
-
